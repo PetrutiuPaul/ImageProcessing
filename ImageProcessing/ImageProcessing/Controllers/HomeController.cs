8be0f309f8989f965b398945872d4f6b0a77f727
PetrutiuPaul <paul970410@gmail.com>
2019-05-03 19:04:25 +0300

    contur + subtiere

--- ImageProcessing/ImageProcessing/Controllers/HomeController.cs
+++ ImageProcessing/ImageProcessing/Controllers/HomeController.cs
@@ -4,8 +4,10 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using Microsoft.Ajax.Utilities;
 
 namespace ImageProcessing.Controllers
@@ -156,6 +158,51 @@ namespace ImageProcessing.Controllers
             return RedirectToAction("Index");
         }
 
+
+
+        public ActionResult Contur()
+        {
+            foreach (var file in Directory.GetFiles(Server.MapPath("~/UploadedFiles")))
+            {
+                if (!file.Contains("current")) continue;
+                using (var bitmap = new Bitmap(file))
+                {
+                    using (var bitmap2 = new Bitmap(file))
+                    {
+                        for (var i = 1; i < bitmap.Width - 1; i++)
+                        {
+                            for (var j = 1; j < bitmap.Height - 1; j++)
+                            {
+                                bitmap2.SetPixel(i, j,
+                                    bitmap.GetPixel(i, j).Name.Contains("000000")
+                                        ? Contur(bitmap, i, j)
+                                        : Color.White);
+                            }
+                        }
+                        bitmap2.Save(Server.MapPath($"~/UploadedFiles/result-{DateTime.Now.Ticks}" + Path.GetExtension(file)));
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Subtiere()
+        {
+            foreach (var file in Directory.GetFiles(Server.MapPath("~/UploadedFiles")))
+            {
+                if (!file.Contains("current")) continue;
+                using (var bitmap = new Bitmap(file))
+                {
+                    var bitmap2 = Subtiere(bitmap, file);
+                    bitmap2.Save(
+                        Server.MapPath($"~/UploadedFiles/result-{DateTime.Now.Ticks}" + Path.GetExtension(file)));
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
         private static Color MidColor(Bitmap bitmap, int i, int j)
         {
             var r = 0;
@@ -195,6 +242,145 @@ namespace ImageProcessing.Controllers
             return Color.FromArgb(pixel.R - (r / 9) < 0 ? 0 : pixel.R - (r / 9) > 255 ? 255 : pixel.R - (r / 9),
                                 pixel.G - (g / 9) < 0 ? 0 : pixel.G - (g / 9) > 255 ? 255 : pixel.G - (g / 9),
                                 pixel.B - (b / 9) < 0 ? 0 : pixel.B - (b / 9) > 255 ? 255 : pixel.B - (b / 9));
+        }
+
+        private static Color Contur(Bitmap bitmap, int i, int j)
+        {
+            var pixel = bitmap.GetPixel(i, j);
+            var r = 0;
+            var g = 0;
+            var b = 0;
+
+            for (var ii = -1; ii <= 1; ii++)
+            {
+                for (var jj = -1; jj <= 1; jj++)
+                {
+                    if (((ii == 0 && jj != 0) || (ii != 0 && jj == 0) || (ii != 0 && jj !=0)) && bitmap.GetPixel(i + ii, j + jj).Name.Contains("ffffff"))
+                        return Color.Red;
+                }
+            }
+
+            return Color.Black;
+        }
+
+
+        private static Bitmap Subtiere(Bitmap bitmap, string file)
+        {
+            var change = true;
+            var bitmap2 = new Bitmap(bitmap);
+            while (change)
+            {
+                change = false;
+                
+                    for (var i = 1; i < bitmap.Width - 1; i++)
+                    {
+                        for (var j = 1; j < bitmap.Height - 1; j++)
+                        {
+                            bitmap.SetPixel(i, j,
+                                bitmap.GetPixel(i, j).Name.Contains("000000")
+                                    ? Contur(bitmap, i, j)
+                                    : bitmap.GetPixel(i, j));
+                        }
+                    }
+                    
+
+                    for (var i = bitmap.Width - 2; i >= 1; i--)
+                    {
+                        for (var j = bitmap.Height - 2; j >= 1; j--)
+                        {
+                            if (bitmap.GetPixel(i, j).Name.Contains("ffff0000"))
+                            {
+                                var nv = 0;
+
+                                for (var ii = -1; ii <= 1; ii++)
+                                {
+                                    for (var jj = -1; jj <= 1; jj++)
+                                    {
+                                        if (ii == jj && ii == 0) continue; 
+                                        if (bitmap.GetPixel(i + ii, j + jj).Name.Contains("ff00"))
+                                            nv++;
+                                    }
+                                }
+
+                                if (nv >= 2 && nv <= 6 && Limit(bitmap,i,j))
+                                {
+                                    bitmap2.SetPixel(i, j, Color.White);
+                                    change = true;
+                                }
+                                else
+                                {
+                                    bitmap2.SetPixel(i, j, bitmap.GetPixel(i,j).Name.Contains("ffff0000")?Color.Black:bitmap.GetPixel(i,j));
+                                }
+                            }
+                            else
+                            {
+                                bitmap2.SetPixel(i, j, bitmap.GetPixel(i, j));
+                            }
+                        }
+                    }
+                    bitmap = new Bitmap(bitmap2);
+            }
+
+            return bitmap2;
+        }
+
+        private static bool Limit(Bitmap bitmap, int i, int j)
+        {
+            var queue = new Queue<KeyValuePair<int, int>>();
+
+
+            var dx = new List<int> {-1, -1, 0, 1, 1, 1, 0, -1 , -1};
+            var dy = new List<int> {0, 1, 1, 1, 0, -1, -1, -1, 0};
+
+            var transition = 0;
+
+            for (var it = 8; it >= 1; it--)
+            {
+                if (bitmap.GetPixel(i + dx[it], j + dy[it]).Name.Contains("ff00") &&
+                    !bitmap.GetPixel(i + dx[it - 1], j + dy[it - 1]).Name.Contains("ff00"))
+                    transition++;
+            }
+
+            return transition == 1;
+
+
+            //using (var bitmap2 = new Bitmap(bitmap))
+            //{
+
+            //    for (var it = 0; it < 4; it++)
+            //    {
+            //        if (!bitmap2.GetPixel(i + dx[it], j + dy[it]).Name.Contains("ff00")) continue;
+            //        queue.Enqueue(new KeyValuePair<int, int>(i + dx[it], j + dy[it]));    
+            //        break;
+            //    }
+
+
+            //    while (queue.Count > 0)
+            //    {
+            //        var current = queue.Dequeue();
+            //        if (!bitmap2.GetPixel(current.Key, current.Value).Name.Contains("fffffff"))
+            //        {
+            //            bitmap2.SetPixel(current.Key, current.Value, Color.White);
+
+
+            //            for (var it = 0; it < 4; it++)
+            //            {
+            //                if (!bitmap2.GetPixel(current.Key + dx[it], current.Value + dy[it]).Name.Contains("ff00"))
+            //                    continue;
+            //                queue.Enqueue(new KeyValuePair<int, int>(current.Key + dx[it], current.Value + dy[it]));
+            //            }
+            //        }
+            //    }
+
+            //    for (var it = 0; it < 4; it++)
+            //    {
+            //        if (bitmap2.GetPixel(i + dx[it], j + dy[it]).Name.Contains("ff00"))
+            //            return false;
+            //    }
+
+            //}
+
+            //return true;
         }
 
         private static Color BiomedColor(Bitmap bitmap, int i, int j)
